b694ef028060b0adef4445fea5a1a1a0bfe6f98b
adniltonsantos <adniltonweb@gmail.com>
2020-08-14 01:01:47 -0300

    update

--- topo.php
+++ topo.php
@@ -6,7 +6,7 @@
 		<meta name="viewport" content="width=device-width,initial-scale1">
     
     <title>Intranet</title>    
-	<!-- Logo do Sistema -->
+	<!-- Logo do Sistema na aba -->
 	<link rel="icon" href="img/logo.png" type="image/gif" sizes="16x16">
     <!-- Link do Java e Css do Bootsrap -->
     <script type="text/javascript" src="js/jquery.js"></script>
